a93ec76921cee169634dda4387189291c4da8185
Darley Leal <108217516+DarleyLeal@users.noreply.github.com>
2023-09-30 10:24:20 -0300

    Add project files.

---
+++ CSharpCourse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite sua senha: ");
+            double senha = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha inválida, tente novamente!");
+                senha = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
